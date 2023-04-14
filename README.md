785289696301eb8b6deeb614418dd32592dca42b
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-04-14 12:05:58 +0200

    Add quiz functionality to Python program, with questions and answers stored in JSON file and update whole code. Update README.md and add file questions.json

--- README.md
+++ README.md
@@ -9,10 +9,36 @@ The application requires Python 3.x.
 ## Usage
 
 1. Clone the repository or download and extract the archive.
-2. Open a command prompt or terminal and navigate to the directory where the `main.py` file is located.
-3. Run the program by typing `python main.py` in the command prompt or terminal.
-4. Take the quiz by selecting the number of the answer and pressing Enter.
+2. Open a command prompt or terminal and navigate to the directory where the main.py file is located.
+3. Run the program by typing python main.py in the command prompt or terminal.
+4. Take the quiz by selecting the number of the answer and pressing Enter. If an invalid answer number is entered, the user will be prompted to enter the answer again.
 5. After finishing the quiz, the user's score will be displayed.
+
+## jSON Format
+
+The questions are stored in a JSON file with the following format:
+
+```json
+{
+    "questions": [
+        {
+            "text": "What is the capital of France?",
+            "options": ["London", "Paris", "Berlin", "Madrid"],
+            "answer": "Paris"
+        },
+        {
+            "text": "What is the largest planet in our solar system?",
+            "options": ["Mars", "Jupiter", "Saturn", "Neptune"],
+            "answer": "Jupiter"
+        },
+        ...
+    ]
+}
+```
+The "questions" key contains an array of question objects. Each question object has the following keys:
+* "text": the text of the question.
+* "options": an array of possible answer options.
+* "answer": the correct answer in the "options" array.
 
 ## License
 
--- main.py
+++ main.py
@@ -1,40 +1,31 @@
-import random
+import json
 
-questions = [
-    {
-        "question": "What is the capital of France?",
-        "answers": ["Paris", "London", "Berlin", "Madrid"]
-    },
-    {
-        "question": "What is the largest country in the world?",
-        "answers": ["Russia", "China", "USA", "Canada"]
-    },
-    {
-        "question": "What is the currency of Japan?",
-        "answers": ["Yen", "Dollar", "Euro", "Pound"]
-    }
-]
+with open('questions.json', 'r') as f:
+    questions = json.load(f)
 
-random.shuffle(questions)  # losowe ustawienie pytań
+score = 0
+total_questions = len(questions)
 
-score = 0  # wynik użytkownika
+for i, question in enumerate(questions):
+    print(f'Question {i+1}/{total_questions}: {question["question"]}')
+    for j, answer in enumerate(question['answers']):
+        print(f'{j+1}. {answer}')
 
-for q in questions:
-    print(q["question"])
-    for i, a in enumerate(q["answers"]):
-        print(f"{i + 1}. {a}")
-    user_answer_valid = False
-    while not user_answer_valid:
-        user_answer = input("Enter the number of your answer: ")
-        if not user_answer.isdigit() or int(user_answer) < 1 or int(user_answer) > len(q["answers"]):
-            print("Invalid answer! Please choose a valid answer number.")
-        elif q["answers"][int(user_answer) - 1] == q["answers"][0]:
-            print("Correct answer!")
+    while True:
+        user_answer = input('Enter the correct answer number: ')
+        if not user_answer.isdigit():
+            print('Invalid input. Please enter a number.')
+            continue
+        user_answer = int(user_answer)
+        if user_answer < 1 or user_answer > len(question['answers']):
+            print(f'Invalid input. Please enter a number between 1 and {len(question["answers"])}')
+            continue
+        if question['answers'][user_answer-1] == question['correct_answer']:
+            print('Correct answer!')
             score += 1
-            user_answer_valid = True
+            break
         else:
-            print("Wrong answer! Please try again.")
+            print('Incorrect answer.')
+            break
 
-print(f"Your score: {score} out of {len(questions)}")
-
-print("Your score is " + str(score) + " out of " + str(len(questions)) + " possible points.")
+print(f'You scored {score}/{total_questions}')
